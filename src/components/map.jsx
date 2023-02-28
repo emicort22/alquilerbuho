1cca3e866510aab37dbb48567d007b53d4ec5eaf
maria emilia <mecortegozo@gmail.com>
2023-02-28 09:25:34 -0300

    f

---
+++ src/components/map.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d108929.40899193358!2d-64.56783069533473!3d-31.440454748375483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x942d67b46f1baf03%3A0x6ac716c622f55f1!2sEl%20Bhuo%20Departamentos!5e0!3m2!1ses!2sar!4v1677495047412!5m2!1ses!2sar"width="300" height="300"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  )
+}
+
+export default Map
